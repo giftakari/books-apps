a172f067f243ea29c8499a1cad80247b8e426d35
Gift <giftakari@yahoo.com>
2022-03-25 07:11:41 +1000

    public routes setup

--- book-store/src/app/auth/components/login/login.component.ts
+++ book-store/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  name:string ="";
+  password:string=""
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(queryParam=> {
+      this.name = queryParam['name'];
+      this.password = queryParam['password']
+    })
   }
 
 }
