a5077821ffad262b233566c45143c7581b9ba3e9
Gift <giftakari@yahoo.com>
2022-03-24 17:58:02 +1000

    user module setup

--- book-store/src/app/user/user.module.ts
+++ book-store/src/app/user/user.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { AddBookComponent } from './components/add-book/add-book.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
+import { UserComponent } from './user.component';
 
 
 @NgModule({
   declarations: [
-    AddBookComponent
+    AddBookComponent,
+    DeleteBookComponent,
+    UserComponent
   ],
   imports: [
     CommonModule,
